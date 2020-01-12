936699c36d4ab6d466ce8449598d3642926d4f0a
Sylvain Boily <sylvain@wazo.io>
2020-01-12 12:31:59 -0500

    add call user, work on wazo node

---
+++ nodes/call_user.js
@@ -0,0 +1,102 @@
+module.exports = function (RED) {
+  const { WazoApiClient } = require('@wazo/sdk');
+  const fetch = require('node-fetch');
+  const https = require("https");
+
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+
+  function call_user(n) {
+    RED.nodes.createNode(this, n);
+    this.user_uuid = n.user_uuid;
+    wazoConn = RED.nodes.getNode(n.server);
+    this.client = wazoConn.client.calld;
+
+    var node = this;
+
+    node.on('input', async msg => {
+      node_uuid = msg.payload.node_uuid;
+      application_uuid = msg.payload.application_uuid;
+
+      if (!node_uuid && !application_uuid) { return; }
+
+      if (!conn.client.client.token) {
+        await conn.authenticate();
+      }
+      const token = conn.client.client.token;
+      if (node_uuid) {
+        const url = `https://${conn.host}:${conn.port}/api/calld/1.0/applications/${application_uuid}/nodes/${node_uuid}/calls/user`;
+        try {
+          const { ...call_user} = await initiateCallUser(url, token, node.user_uuid);
+          node.log(`Call user ${node.user_uuid} to node ${node_uuid}`);
+          node.send(call_user);
+        }
+        catch(err) {
+          node.error(err);
+        }
+      }
+    });
+
+  }
+
+  // FIXME: Remove when SDK will be ready
+  async function initiateCallUser(url, token, user_uuid) {
+    const body = {
+      user_uuid: user_uuid
+    }
+
+    const options = {
+        method: 'POST',
+        agent: agent,
+        body: JSON.stringify(body),
+        headers: {
+          'content-type': 'application/json',
+          'X-Auth-Token': token
+        }
+    }
+
+    return fetch(url, options).then(response => response.json()).then(data => data);
+  }
+
+  // FIXME: Remove when SDK will be ready
+  async function listNodes(url, token) {
+    const options = {
+        method: 'GET',
+        agent: agent,
+        headers: {
+          'content-type': 'application/json',
+          'X-Auth-Token': token
+        }
+    };
+
+    return fetch(url, options).then(response => response.json()).then(data => data);
+  }
+
+  RED.httpAdmin.post('/wazo-platform/users', RED.auth.needsPermission('wazo.write'), async function(req, res) {
+    client = new WazoApiClient({
+      server: `${req.body.host}:${req.body.port}`,
+      agent: agent,
+      clientId: 'wazo-nodered'
+    });
+
+    try {
+       const { ...authentication } = await client.auth.refreshToken(req.body.refreshToken);
+       client.setToken(authentication.token);
+    }
+    catch(err) {
+      res.send(err);
+    }
+
+    try {
+      const { ...users } = await client.confd.listUsers();
+      res.json(users);
+    }
+    catch(err) {
+      res.send(err);
+    }
+  });
+
+  RED.nodes.registerType("wazo call user", call_user);
+
+}
--- nodes/node.js
+++ nodes/node.js
@@ -1,29 +1,90 @@
 module.exports = function (RED) {
+  const { WazoApiClient } = require('@wazo/sdk');
+  const fetch = require('node-fetch');
+  const https = require("https");
+
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
     
   function node(n) {
     RED.nodes.createNode(this, n);
-    wazoConn = RED.nodes.getNode(n.server);
+    conn = RED.nodes.getNode(n.server);
     this.node_name = n.node_name;
     this.node_uuid = n.node_uuid;
-    this.client = wazoConn.client.application;
+    this.refreshToken = n.refreshToken;
+    this.client = conn.client.application;
 
     var node = this;
 
-    node.on('input', msg => {
-      call_id = msg.data.call.id;
-      application_uuid = msg.data.application_uuid;
+    node.on('input', async msg => {
+      if (msg.payload.call.id) {
+        call_id = msg.payload.call.id;
+        application_uuid = msg.payload.application_uuid;
+        node.log("Call will entered to a node");
 
-      if (call_id) {
-        node.log('Add call to node');
-        try {
-          const result = node.client.createNewNodeWithCall(application_uuid, [{id: call_id}]);
-          node.send(msg);
+        if (!conn.client.client.token) {
+          await conn.authenticate();
         }
-        catch(err) {
-          node.error(err);
+        const token = conn.client.client.token;
+
+        if (node.node_uuid) {
+          try {
+            const { ...calls_node} = await node.client.listCallsNodes(application_uuid, node.node_uuid);
+            node.node_uuid = calls_node.uuid;
+          }
+          catch(err) {
+            node.node_uuid = null;
+          }
+        }
+
+        if (node.node_uuid) {
+          try {
+            await node.client.addCallNodes(application_uuid, node.node_uuid, call_id);
+            node.log(`Add call to existing node ${node.node_uuid}`);
+            node.send({payload: {call: msg, node_uuid: node.node_uuid, application_uuid: application_uuid}});
+          }
+          catch(err) {
+            node.error(err);
+          }
+        } else {
+          try {
+            const url = `https://${conn.host}:${conn.port}/api/calld/1.0/applications/${application_uuid}/nodes`;
+            const { ...call_node} = await createNodeAddCall(url, token, call_id);
+            node.log(`Add call to node ${call_node.uuid}`);
+            node.node_uuid = call_node.uuid;
+            call_node.payload = call_node.data;
+            call_node.topic = call_node.name;
+            node.send({payload: {call: msg, node: call_node, node_uuid: node.node_uuid, application_uuid: application_uuid}});
+          }
+          catch(err) {
+            node.error(err);
+          }
         }
       }
-    });  
+    });
+
+  }
+
+  // FIXME: Remove when SDK will be ready
+  async function createNodeAddCall(url, token, call_id) {
+    const body = {
+      calls: [{
+        id: call_id
+      }]
+    }
+
+    const options = {
+        method: 'POST',
+        agent: agent,
+        body: JSON.stringify(body),
+        headers: {
+          'content-type': 'application/json',
+          'X-Auth-Token': token
+        }
+    }
+
+    return fetch(url, options).then(response => response.json()).then(data => data);
   }
 
   RED.httpAdmin.post('/wazo-platform/node', RED.auth.needsPermission('wazo.read'), function(req, res) {
