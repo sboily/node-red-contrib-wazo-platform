446bb3937e4f5f7a9fb0f34439ec01742d90c1e7
Sylvain Boily <sylvain@wazo.io>
2020-03-08 17:47:31 -0400

    refactor

--- nodes/bridge_call.js
+++ nodes/bridge_call.js
@@ -18,7 +18,7 @@ module.exports = function (RED) {
     var node = this;
 
     node.on('input', async msg => {
-      call_id = msg.payload.call.id || msg.payload.call_id;
+      call_id = msg.payload.call ? msg.payload.call.id : msg.payload.call_id;
       application_uuid = msg.payload.application_uuid;
       exten = node.exten || msg.payload.exten;
       context = node.context || msg.payload.context;
